55531d7c653c510e8a6788a772fc4a5e36e5db8d
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2019-01-18 10:18:15 +1100

    publishing build

---
+++ client/build/precache-manifest.229c46acb4497e6ae90ccf8cf21c740d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f5005fb00f5a7062feb",
+    "url": "/static/js/main.9f5005fb.chunk.js"
+  },
+  {
+    "revision": "64fe3e52feaba452b19e",
+    "url": "/static/js/1.64fe3e52.chunk.js"
+  },
+  {
+    "revision": "8982ea3313b6a720a40977befefa7ac1",
+    "url": "/index.html"
+  }
+];
